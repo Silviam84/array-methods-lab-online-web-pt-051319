a35c3a79de1f6a15de476d60ec5aef25dd780943
classy-waterfall-6294 <silviamendoza223@yahoo.com>
2019-05-20 00:42:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,25 +1,25 @@
-def using_include("some" "")
+def using_include("Lace" "Grace")
   .include?
 
 end
 
-def using_sort(array)
+def using_sort(name)
   #sort
 
 end
 
-def using_reverse(array)
+def using_reverse("Grace")
   #reverse
 
 end
 
-def using_first(array)
+def using_first("Lace")
   #firs
   
 
 end
 
-def using_last(array)
+def using_last("Grace) 
   #last
 
 end
